447d861de99959b7579d285862ffd8534b611fcf
Siwontheball <intern12sion@gmail.com>
2026-04-16 00:35:35 +0900

    Revert "created メモ作成画面"

--- laravel/resources/js/App.vue
+++ laravel/resources/js/App.vue
@@ -1,26 +1,9 @@
 <script setup lang="ts">
 import Header from "./components/Header.vue"
-import MemoForm from "./components/MemoForm.vue"
+
 </script>
 
 <template>
-    <div class="app-background">
-        <Header />
-        <MemoForm />
-    </div>
+  <Header />
+
 </template>
-
-<style>
-html, body {
-    margin: 0;
-    padding: 0;
-    height: 100%;
-}
-
-.app-background{
-    background-color: rgb(232, 207, 159, 0.6);
-    min-height: 100vh;
-    width: 100%;
-}
-
-</style>
